1590058aac88c5ec16754003f9fb232e16f250ca
Michiel Thomassen <bunzzbunzz@gmail.com>
2019-07-04 01:47:12 +0200

    It works! the Raw value of Content also had to be set.
    Code cleanup

--- src/main/java/be/voeren2000/bezettingsportkampen/Main.java
+++ src/main/java/be/voeren2000/bezettingsportkampen/Main.java
@@ -10,79 +10,113 @@ import com.afrozaar.wordpress.wpapi.v2.config.ClientFactory;
 import com.afrozaar.wordpress.wpapi.v2.exception.PageNotFoundException;
 import com.afrozaar.wordpress.wpapi.v2.model.Content;
 import com.afrozaar.wordpress.wpapi.v2.model.Page;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.HttpClientErrorException;
 
+import javax.net.ssl.SSLContext;
 import java.io.File;
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 
 public class Main {
     public static void main(String[] args) {
-        /*args = new String[2];
+        args = new String[2];
         args[0] = "Lijst inschr. sportkamp ZOMER 2019.xlsx";
-        args[1] = "Lijst inschr. kleuters ZOMER 2019.xlsx";*/
-    
+        args[1] = "Lijst inschr. kleuters ZOMER 2019.xlsx";
+        
         PropertiesLoader propertiesLoader = new PropertiesLoader();
         
-            Element divContainer = new Element("div");
-            divContainer.attr("id", propertiesLoader.getString("html_container_id"));
-            for (int i = 0; i < args.length; i++) {
-                try (Workbook workbook = WorkbookFactory.create(new File(args[i]))) {
-                    RegistrationSheet registrationSheet = new RegistrationSheet(workbook);
-                    ReadExcelFile file = new ReadExcelFile();
-                    file.readFile(registrationSheet);
-                    HtmlTable htmlTable = new HtmlTable();
-                    String headerName = args[i].split(" ")[2];
-                    headerName = headerName.substring(0,1).toUpperCase() + headerName.substring(1).toLowerCase();
-                    divContainer.appendChild(htmlTable.createH1Header(headerName));
-                    divContainer.appendChild(htmlTable.processRegistrations(registrationSheet));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+        Element divContainer = new Element("div");
+        divContainer.attr("id", propertiesLoader.getString("html_container_id"));
+        for (int i = 0; i < args.length; i++) {
+            try (Workbook workbook = WorkbookFactory.create(new File(args[i]))) {
+                RegistrationSheet registrationSheet = new RegistrationSheet(workbook);
+                ReadExcelFile file = new ReadExcelFile();
+                file.readFile(registrationSheet);
+                HtmlTable htmlTable = new HtmlTable();
+                String headerName = args[i].split(" ")[2];
+                headerName = headerName.substring(0, 1).toUpperCase() + headerName.substring(1).toLowerCase();
+                divContainer.appendChild(htmlTable.createH1Header(headerName));
+                divContainer.appendChild(htmlTable.processRegistrations(registrationSheet));
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            getPage(divContainer);
+        }
+        getPage(divContainer);
     }
     
-    private static Page getPage(Element divContainer) {
-        PropertiesLoader propertiesLoader = new PropertiesLoader();
-        String baseUrl = propertiesLoader.getString("url");
-        String username = propertiesLoader.getString("user");
-        String password = propertiesLoader.getString("password");
-        boolean debug = Boolean.parseBoolean(propertiesLoader.getString("debug"));
-        boolean usePermalinkEndpoint = Boolean.parseBoolean(propertiesLoader.getString("usePermalinkEndpoint"));
-    
-        Wordpress wordpress = ClientFactory.fromConfig(ClientConfig.of(baseUrl, username, password, usePermalinkEndpoint, debug));
-        
+    private static void getPage(Element divContainer) {
+        Wordpress wordpress = controlHttp();
         try {
-            Page page = wordpress.getPage(350l);
+            Page page = wordpress.getPage(350L);
             Content content = page.getContent();
             Document doc = Jsoup.parse(content.getRendered());
             
             removeOldData(doc);
             insertNewData(doc, divContainer);
             
-            content.setRendered(doc.body().toString());
+            content.setRendered(doc.toString());
+            content.setRaw(doc.toString());
             page.setContent(content);
+    
             wordpress.updatePage(page);
-            return page;
-        } catch (PageNotFoundException e) {
+    
+        } catch (PageNotFoundException | HttpClientErrorException e) {
             e.printStackTrace();
-        } catch (HttpClientErrorException hcee) {
-            hcee.printStackTrace();
         }
-        return null;
     }
     
     private static void removeOldData(Document doc) {
-        doc.getElementById("omnisport").remove();
-        doc.getElementById("kleuterkamp").remove();
+        doc.getElementById("sportkampen").remove();
     }
     
     private static void insertNewData(Document doc, Element divContainer) {
-        doc.body().insertChildren(0,divContainer);
+        doc.insertChildren(0, divContainer);
+    }
+    
+    private static Wordpress controlHttp() {
+        try {
+            TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+            
+            SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom()
+                    .loadTrustMaterial(null, acceptingTrustStrategy)
+                    .build();
+            
+            SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
+            
+            CloseableHttpClient httpClient = HttpClients.custom()
+                    .setSSLSocketFactory(csf)
+                    .build();
+            
+            HttpComponentsClientHttpRequestFactory requestFactory =
+                    new HttpComponentsClientHttpRequestFactory();
+            
+            requestFactory.setHttpClient(httpClient);
+            
+            PropertiesLoader propertiesLoader = new PropertiesLoader();
+            String baseUrl = propertiesLoader.getString("url");
+            String username = propertiesLoader.getString("user");
+            String password = propertiesLoader.getString("password");
+            boolean debug = Boolean.parseBoolean(propertiesLoader.getString("debug"));
+            boolean usePermalinkEndpoint = Boolean.parseBoolean(propertiesLoader.getString("usePermalinkEndpoint"));
+            
+            return ClientFactory.builder(ClientConfig.of(baseUrl, username, password, usePermalinkEndpoint, debug))
+                    .withRequestFactory(requestFactory).build();
+        } catch (NoSuchAlgorithmException | KeyStoreException | KeyManagementException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
